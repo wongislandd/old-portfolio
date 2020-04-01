9bc45bf643857f6fbb908253ab6783c4d34b34d9
Christopher Wong <cwong51799@gmail.com>
2020-03-31 20:48:24 -0400

    Updates

--- precache-manifest.a0919392e3034f4128fe621a4ac5a077.js
+++ precache-manifest.a0919392e3034f4128fe621a4ac5a077.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0bef5080e1e9682d7a64e1ea9fb4263f",
+    "revision": "162f59c786f805e2419042ebbbd50643",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.0eaf8e31.chunk.css"
   },
   {
-    "revision": "ea52318fd6775acd48d3",
+    "revision": "bc18c91519d23a30d9c0",
     "url": "/portfolio/static/css/main.dc8a5728.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.84b7f6b7.chunk.js"
   },
   {
-    "revision": "ea52318fd6775acd48d3",
-    "url": "/portfolio/static/js/main.7618349d.chunk.js"
+    "revision": "bc18c91519d23a30d9c0",
+    "url": "/portfolio/static/js/main.89bad418.chunk.js"
   },
   {
     "revision": "76aa8cbd67b5c937ec2c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3c7ee749152c497ccb9215775feae262.js"
+  "/portfolio/precache-manifest.a0919392e3034f4128fe621a4ac5a077.js"
 );
 
 self.addEventListener('message', (event) => {
